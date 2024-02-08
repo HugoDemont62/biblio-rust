91e273f503c279007179a2b4ce3680bf4c621501
HugoDemont <hugodemont62@hotmail.fr>
2024-02-08 21:57:56 +0100

    [FEAT] Update .gitignore, add serialization dependencies, and create book fetching service
    This commit adds serde and serde_derive to Cargo.toml, allowing for data serialization. It presents the `get_books` function in main.rs that serves book data upon route access, and updates .gitignore to include Cargo.lock.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Book {
+    title: String,
+    author: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    "Hello World!"
+    HttpResponse::Ok().body(r#"<a href="/books">Go to books</a>"#)
 }
 
 #[get("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     req_body
+}
+
+#[get("/books")]
+async fn get_books() -> impl Responder {
+    web::Json(vec![
+        Book {
+            title: "Moby Dick".to_string(),
+            author: "Herman Melville".to_string(),
+        },
+        Book {
+            title: "The Great Gatsby".to_string(),
+            author: "F. Scott Fitzgerald".to_string(),
+        },
+    ])
 }
 
 #[actix_web::main]
@@ -18,6 +39,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(get_books)
     })
         .bind("127.0.0.1:8080")?;
 
