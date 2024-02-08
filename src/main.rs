c35682a11c89bf8a3d2494c75dbb982f9e808502
HugoDemont <hugodemont62@hotmail.fr>
2024-02-08 21:50:00 +0100

    [FEAT] Add Actix-web dependency and implement basic server
    This commit introduces the Actix-web dependency in the Cargo.toml file. It also changes the main.rs file with an asynchronous "main" function to set up a basic Actix-web server along with two routes: '/', which responds with "Hello World!", and '/echo', which mirrors (echoes) received input back to the sender.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    "Hello World!"
+}
+
+#[get("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    req_body
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server is starting...");
+
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+        .bind("127.0.0.1:8080")?;
+
+    let server = server.run();
+
+    std::thread::spawn(|| {
+        println!("Server has started.");
+    });
+
+    server.await
 }
