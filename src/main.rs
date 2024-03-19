7af5d0cbbac5dff16a4933fa99655b14964eb92e
HugoDemont62 <hugodemontpro@gmail.com>
2024-03-19 09:43:50 +0100

    [FEAT] Updated comments
    All comments from main.rs have been removed for better readability in the code.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ mod pages {
     pub mod books;
     pub mod echo;
     pub mod home;
-    pub mod add_book; // Assurez-vous d'avoir ce module
+    pub mod add_book;
 }
 
 use actix_web::{App, HttpServer};
 use pages::books::get_books;
 use pages::echo::echo;
 use pages::home::get_home;
-use pages::add_book::get_add_book; // Assurez-vous d'avoir cette fonction
+use pages::add_book::get_add_book;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,7 +21,7 @@ async fn main() -> std::io::Result<()> {
             .service(get_books)
             .service(echo)
             .service(get_home)
-            .service(get_add_book) // Assurez-vous d'avoir ce service
+            .service(get_add_book)
     })
         .bind("127.0.0.1:8080")?;
 
